618373d4eef5b99f3866c694e4754bab1f7527cc
Jakob Fix <jakob@jfix.com>
2016-10-20 16:35:09 +0200

    some changes
    now firing for new issues and for changed issues. some tiny newbie
    refactoring going because of that.
    
    TODO: make certain things configurable (broker URL mainly)

--- init.rb
+++ init.rb
@@ -2,9 +2,9 @@ require_dependency 'issues_hook_listener'
 
 Redmine::Plugin.register :notify do
   name 'Notify plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  author 'mereth & jfix'
+  description 'This is a plugin to send notifications to a message broker'
+  version '0.0.7'
+  url 'https://github.com/mereth/redmine-notify'
+  author_url 'https://github.com/mereth'
 end
--- lib/issues_hook_listener.rb
+++ lib/issues_hook_listener.rb
@@ -1,25 +1,52 @@
 
 class IssuesHookListener < Redmine::Hook::Listener
-  def controller_issues_edit_after_save(context = {})
+  @@broker_url = 'http://localhost:8161/api/message/redmine?type=topic'
+
+  # catch new tickets
+  def controller_issues_new_after_save(context = {})
     issue = context[:issue]
-	journal = context[:journal]
-	
-	statusChange = journal.detail_for_attribute('status_id')
-	
-	if statusChange.nil?
-		return
-	end
-	
-	event = {:issue => issue.id,:old_status => statusChange.old_value,:new_status => statusChange.value}
-	
-	Thread.new do
-		uri = URI.parse('http://httpbin.org/post')
-		request = Net::HTTP::Post.new(uri, {'Content-Type' => 'application/json'})
-		request.body = event.to_json
-		
-		http = Net::HTTP.new(uri.host, uri.port)
-		http.request(request)
-	end
+
+    event = {
+      :type => "creation",
+      :issue => issue.id,
+      :project_id => issue.project.id,
+      :priority => issue.priority.id
+    }
+
+    send_event(event)
   end
 
+  # catch ticket status changes
+  def controller_issues_edit_after_save(context = {})
+    issue = context[:issue]
+    journal = context[:journal]
+    statusChange = journal.detail_for_attribute('status_id')
+
+    if statusChange.nil?
+      return
+    end
+
+    event = {
+      :type => "change",
+      :issue => issue.id,
+      :project_id => issue.project.id,
+      :priority => issue.priority.id,
+      :old_status => statusChange.old_value,
+      :new_status => statusChange.value
+    }
+    send_event(event)
+  end
+
+  # method to send the event to the message broker
+  def send_event(event)
+    Thread.new do
+      uri = URI.parse(@@broker_url)
+      request = Net::HTTP::Post.new(uri, {'Content-Type' => 'application/json'})
+      request.body = event.to_json
+      request.basic_auth("admin", "admin")
+
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.request(request)
+    end
+  end
 end
