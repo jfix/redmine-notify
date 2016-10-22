a63f2ae8cb6653f3c9e9286f9b7b0c35c169cf56
Jakob Fix <jakob@jfix.com>
2016-10-22 21:29:10 +0200

    settings now configurable
    There is now a settings page for URL, login and password for the
    message broker.
    
    TODO: provide a means to test that the settings work as intended (have
    a "test" button to make a call to the URL with the credentials provided)

--- lib/issues_hook_listener.rb
+++ lib/issues_hook_listener.rb
@@ -1,8 +1,6 @@
 
 class IssuesHookListener < Redmine::Hook::Listener
-  
-  @@broker_url = 'http://localhost:8161/api/message/redmine?type=topic'
-  
+
   # catch new tickets
   def controller_issues_new_after_save(context = {})
     issue = context[:issue]
@@ -16,18 +14,18 @@ class IssuesHookListener < Redmine::Hook::Listener
 
     send_event_async(event)
   end
-  
+
   # catch ticket status changes
   def controller_issues_edit_after_save(context = {})
     issue = context[:issue]
     journal = context[:journal]
-    
+
     statusChange = journal.detail_for_attribute('status_id')
-    
+
     if statusChange.nil?
       return
     end
-    
+
     event = {
       :type => "change",
       :issue_id => issue.id,
@@ -36,18 +34,24 @@ class IssuesHookListener < Redmine::Hook::Listener
       :old_status_id => statusChange.old_value,
       :new_status_id => statusChange.value
     }
-    
+
     send_event_async(event)
   end
 
   # method to send the event to the message broker
   def send_event_async(event)
+
+    # these variables are retrieved from the settings (see _notify_settings.html.erb)
+    @@broker_url = Setting.plugin_notify[:broker_url]
+    @@broker_login = Setting.plugin_notify[:broker_login]
+    @@broker_pwd = Setting.plugin_notify[:broker_pwd]
+
     Thread.new do
       uri = URI.parse(@@broker_url)
-      
+
       # load cookies store
       cookiesStore = PStore.new("notify.cookies.pstore", thread_safe = true)
-      
+
       # prepare cookie header
       cookiesString = ""
       cookiesStore.transaction(true) do
@@ -56,19 +60,19 @@ class IssuesHookListener < Redmine::Hook::Listener
           cookiesString = cookies.map{|k,v| "#{k}=#{v}"}.join(';')
         end
       end
-      
+
       # setup request
       request = Net::HTTP::Post.new(uri, {
         'Content-Type' => 'application/json',
         'Cookie' => cookiesString
       })
-      request.basic_auth("admin", "admin")
+      request.basic_auth(@@broker_login, @@broker_pwd)
       request.body = event.to_json
-      
+
       # execute request
       http = Net::HTTP.new(uri.host, uri.port)
       response = http.request(request)
-      
+
       # save any new cookies
       newCookies = response.get_fields('Set-Cookie')
       if(newCookies)
@@ -77,12 +81,12 @@ class IssuesHookListener < Redmine::Hook::Listener
           if(!cookies)
             cookies = Hash.new
           end
-          
+
           newCookies.each do |cookie|
             cookie = ( cookie.split(';')[0] ).split('=', 2)
             cookies[ cookie[0] ] = cookie[1]
           end
-          
+
           cookiesStore[:cookies] = cookies
         end
       end
